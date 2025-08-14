643aaca93168d0043d2c8a913057893c0e030086
Malakkhaled1 <malakelhady76@gmail.com>
2025-08-14 13:14:51 +0300

    fix: Auto wired with service interface

--- src/main/java/com/Ejada/SpringTask/APIs/Controllers/UserController.java
+++ src/main/java/com/Ejada/SpringTask/APIs/Controllers/UserController.java
@@ -3,6 +3,7 @@ package com.Ejada.SpringTask.APIs.Controllers;
 
 import com.Ejada.SpringTask.Application.Models.User;
 import com.Ejada.SpringTask.Application.Services.Impl.UserServicesImpl;
+import com.Ejada.SpringTask.Application.Services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private UserServicesImpl userServices;
+    private UserServices userServices;
 
     @GetMapping("/user")
     public void getAllUsers() {
